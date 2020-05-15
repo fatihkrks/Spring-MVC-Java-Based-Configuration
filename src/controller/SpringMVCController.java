e3689b5a02f8ca82e07c33449cd773e61d45a404
Fatih Karakas <fatihkrks95@gmail.com>
2020-05-15 14:26:28 +0300

    SpringMVC

---
+++ src/controller/SpringMVCController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import service.MyService;
+
+@Controller
+public class SpringMVCController {
+
+	@Autowired
+	private MyService myService;
+
+	@RequestMapping(value = "/message", method = RequestMethod.GET)
+	public String message(ModelMap modelMap) {
+		String msg = myService.Message();
+		modelMap.addAttribute("myAttribute", msg);
+		return "helloPage";
+	}
+}
